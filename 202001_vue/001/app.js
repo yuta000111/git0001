81d2dec07f60605067cce3ba61fa0972dd520392
ramirez <yuta.yamazaki@infobahn.co.jp>
2020-11-20 19:00:21 +0900

    Todotitle,categoryの監視 methodからcomputedの呼び出しで入力されたコンテンツの確認

--- 202001_vue/001/app.js
+++ 202001_vue/001/app.js
@@ -5,14 +5,51 @@ Vue.createApp({
             todoTitle: '',
             todoDescription: '',
             todoCategories: [],
+            selectedCategory: '',
+            todos: [],
+            categories: [],
             hideDoneTodo: false,
             searchWord: '',
             order: 'desc',
+            categoryName: '',
         }
     },
     conputet: {
         canCreatedTodo: function() {
             return this.todoTitle !== ''
         },
-    }
+        canCreateCategory: function() {
+            return this.categoryName !== '' && !this.existsCategory
+        },
+        existsCategory: function() {
+            const categoryName = this.categoryName
+
+            return this.categories.indexOf(categoryName) !== -1
+        },
+    },
+    methods: {
+        createTodo: function() {
+            if (!this.canCreatedTodo) {
+                return
+            }
+            this.todos.push({
+                id: 'todo-' + Date.now(),
+                title: this.todoTitle,
+                desicriptioin: this.todoDescription,
+                categories: this.todoCategories,
+                dateTime: Date.now(),
+                done: false,
+            })
+            this.todoTitle = ''
+            this.todoDescription = ''
+            this.todoCategories = []
+        },
+        CreateCategory: function() {
+            if (!this.canCreateCategory) {
+                return
+            }
+            this.categories.push(this.categoryName)
+            this.categoryName = ''
+        },
+    },
 }).mount('#app')
