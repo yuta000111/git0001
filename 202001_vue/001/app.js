e697cc69be41e3ecd4046661fe8f2b1268faa6b1
ramirez <yuta.yamazaki@infobahn.co.jp>
2020-11-22 22:42:09 +0900

    watch v-bindの話

--- 202001_vue/001/app.js
+++ 202001_vue/001/app.js
@@ -27,6 +27,19 @@ Vue.createApp({
             return this.categories.indexOf(categoryName) !== -1
         },
     },
+    watch: {
+        todos: {
+            hander: function(next) {
+                window.localStorage.setItem('todos', JSON.stringify(next))
+            },
+            deep: true,
+        },
+        categories: {
+            hander: function() {
+                window.localStorage.setItem('categories', JSON.stringify(next))
+            }
+        },
+    },
     methods: {
         createTodo: function() {
             if (!this.canCreatedTodo) {
@@ -50,6 +63,18 @@ Vue.createApp({
             }
             this.categories.push(this.categoryName)
             this.categoryName = ''
+        },
+        created: function() {
+            const todos = window.localStorage.getItem('todos')
+            const categories = window.localStorage.getItem('categories')
+
+            if (todos) {
+                this.todos = JSON.parse(todos)
+            }
+
+            if (categories) {
+                this.categories = JSON.parse(categories)
+            }
         },
     },
 }).mount('#app')
