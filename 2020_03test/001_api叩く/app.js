b0009e231dea28fa856d69e48a208c1342d5042f
ramirez <yuta.yamazaki@infobahn.co.jp>
2020-12-09 23:08:15 +0900

    API実装①

--- 2020_03test/001_api叩く/app.js
+++ 2020_03test/001_api叩く/app.js
@@ -1,100 +1,30 @@
 Vue.createApp({
     data: function() {
         return {
-            // 商品一覧
+            apkey: '0170444aa79e9312a6d820f571dab4dc',
             items: [],
-            activeTab: 1, // アクティブなタブ
-            // 絞り込みテキスト
-            filterText: "",
-            prices: [{
-                    id: 1,
-                    text: "～100円"
-                },
-                {
-                    id: 2,
-                    text: "101円～500円"
-                },
-                {
-                    id: 3,
-                    text: "501円～"
-                }
-            ],
-            // 絞り込み価格ID
-            filterPriceId: undefined
-        }
-    },
-    computed: {
-        // 商品名で絞り込むタブがアクティブ
-        acitiveWordsTab: function() {
-            return this.activeTab === 1
-        },
-        // 価格で絞り込むタブがアクティブ
-        acitivePriceTab: function() {
-            return this.activeTab === 2
-        },
-        // 商品名で絞り込み込みした商品一覧
-        filteredItemsByText: function() {
-            const self = this
-            if (this.filterText) {
-                return this.items.filter(function(item) {
-                    return item.name.indexOf(self.filterText.trim()) !== -1
-                })
-            } else {
-                return this.items
-            }
-        },
-        // 価格で絞り込み込みした商品一覧
-        filteredItemsByPriceId: function() {
-            const self = this
-            if (this.filterPriceId) {
-                return this.items.filter(function(item) {
-                    switch (self.filterPriceId) {
-                        case 1:
-                            return item.price <= 100
-                        case 2:
-                            return item.price > 100 && item.price <= 500
-                        case 3:
-                            return item.price > 500
-                        default:
-                            return item.price >= 0
-                    }
-                })
-            } else {
-                return this.items
-            }
-        },
-        // アクティブなタブの状態を判別して商品一覧を出し分ける
-        filteredItems: function() {
-            if (this.acitiveWordsTab) {
-                return this.filteredItemsByText
-            } else if (this.acitivePriceTab) {
-                return this.filteredItemsByPriceId
-            } else {
-                return this.items
-            }
+            searchWord: "",
+            isHovered: false,
+            hoverdItem: ""
         }
     },
     methods: {
-        // 商品一覧をjsonから取得する
-        fetchItem: function() {
+        fetchApi: function() {
             const self = this
-            axios.get("./item.json").then(function(response) {
-                self.items = response.data
-            })
+            fetch("https://api.search.nicovideo.jp/api/v2/snapshot/video/contents/search?q=" + this.searchWord + "&fields=title,viewCounter,commentCounter,description,tags&targets=title,description,tags&_sort=-viewCounter")
+                .then(response => {
+                    return response.json()
+                })
+                .then(resulet => {
+                    self.items = resulet.data
+                })
         },
-        // タブを切り替えて検索条件を初期化する
-        changeTab: function(number) {
-            this.initialize()
-            this.activeTab = number
-        },
-        // 検索条件を初期化する
-        initialize: function() {
-            this.filterText = ""
-            this.filterPriceId = undefined
+        searching: function() {
+            this.fetchApi()
         }
+
     },
     mounted: function() {
-        // アプリケーションが立ち上がったら商品一覧を取得する
-        this.fetchItem()
+        this.fetchApi()
     }
-}).mount("#app")
+}).mount('#app')
