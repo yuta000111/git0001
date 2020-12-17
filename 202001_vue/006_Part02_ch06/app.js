46d12ef079adeb936b1541d5d75ff3fb70b2e38a
ramirez <yuta.yamazaki@infobahn.co.jp>
2020-12-17 23:29:12 +0900

    part2ch06

--- 202001_vue/006_Part02_ch06/app.js
+++ 202001_vue/006_Part02_ch06/app.js
@@ -3,7 +3,54 @@ const INCORRECT = { CD: 0, VALUE: "☓" }
 const MAX_POINT = 100
 const ANSWER = { YES: 1, NO: 0 }
 
-const app = Vue.createApp({})
+const app = Vue.createApp({
+    data: function() {
+        return {
+            questions: [{
+                text: "v-on:clickの省略記法は #clickである",
+                answer: ANSWER.NO
+            }, ],
+            currentIndex: 0,
+            yourAnswer: []
+        }
+    },
+    computed: {
+        currentQuestion: function() {
+            return this.questions[this.currentIndex]
+        },
+        currentAnswers: function() {
+            const self = this
+            return this.questions.filter(function(quetion, index) {
+                return quetion.answer === self.yourAnswer[index]
+            })
+        },
+        totalScore: function() {
+            const score = MAX_POINT / this.questions.length
+            return Math.floor(score * this.correctAnswers.length)
+        }
+    },
+    methods: {
+        doAnswer: function(answer) {
+            this.yourAnswers[this.currentIndex] = answer
+            this.nextQuestion()
+        },
+        nextQuestion: function() {
+            if (this.currentIndex < this.questions.length) {
+                this.currentIndex += 1
+            }
+        },
+        doRestart: function() {
+            this.currentIndex = 0
+            this.initYourAnswerArray()
+        },
+        initYourAnswerArray: function() {
+            this.yourAnswer = Array(this.questons.length)
+        }
+    },
+    mounted: function() {
+        this.initYourAnswerArray()
+    }
+})
 app.component("result-template", {
     props: {
         totalScore: {
