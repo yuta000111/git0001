19df0c105e50b8b1d55a0971d46064f6f9730d13
ramirez <yuta.yamazaki@infobahn.co.jp>
2021-02-02 23:34:29 +0900

    クイズ実装 クイズコンポーネント

---
+++ vue-book/src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <h1>Vueクイズ</h1>
+  <quiz></quiz>
+</template>
+
+<script>
+import quiz from '@/components/quiz'
+const ANSWER = { YES: 1, NO: 0 }
+const CORRECT = { CD: 1, VALUE: "○" }
+const INCORRECT = { CD: 0, VALUE: "×" }
+const MAX_POINT = 100
+export default {
+  components:{
+    quiz
+  },
+  data:function(){
+    return{
+       questions: [{
+                    text: "v-on:click の省略記法は #click である",
+                    answer: ANSWER.NO
+                },
+                {
+                    text: "単一要素にトランジション効果を付与するのは aniamtion コンポーネントである",
+                    answer: ANSWER.NO
+                },
+                {
+                    text: "v-for の区切り文字として in の他に of を使用できる",
+                    answer: ANSWER.YES
+                },
+                {
+                    text: "app.component で定義したコンポーネントを使う場合は、定義した後に Vue.createApp() の components オプション内で再度定義する必要がある",
+                    answer: ANSWER.NO
+                },
+                {
+                    text: "Vue.js をもっと書きたい",
+                    answer: ANSWER.YES
+                }
+            ],
+            currentIndex:0
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+@import "../node_modules/bulma/bulma.sass";
+
+</style>
---
+++ vue-book/src/components/quiz.vue
@@ -0,0 +1,41 @@
+<template>
+  <h1>Vueクイズ</h1>
+  <div class="box">
+      <div class="content">
+          <p class="is-size-4">Q:{{question}}</p>
+          <div class="columns">
+              <div class="column">
+                  <button class="button is-fullwidth is-success" @click="clickYesBtn">YES</button>
+                  <button class="button is-fullwidth is-warning" @click="clickNoBtn">NO</button>
+              </div>
+          </div>
+      </div>
+  </div>
+</template>
+
+<script>
+const ANSWER = {YES:1,NO:0};
+export default {
+    props:{
+        question:String
+    },
+    data:function(){
+        return{
+            aaa:''
+        }
+    },
+    methods:{
+        clickYesBtn:function(){
+            this.$emit("click-yes-btn",ANSWER.YES)
+        },
+        clickNoBtn:function(){
+            this.$emit("click-no-btn",ANSWER.NO)
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
