accca8cd9983ad9b3efc9c0c37232deed6b40b30
ramirez <yuta.yamazaki@infobahn.co.jp>
2021-02-08 23:53:43 +0900

    nuxt用のvscode設定完了7

---
+++ npm-cafe/.eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+    root: true,
+    env: {
+        browser: true,
+        node: true,
+    },
+    parserOptions: {
+        parser: 'babel-eslint',
+    },
+    extends: [
+        '@nuxtjs',
+        'plugin:prettier/recommended',
+        'plugin:nuxt/recommended',
+        'prettier',
+        'prettier/vue',
+    ],
+    plugins: ['prettier', 'vue'],
+    // add your custom rules here
+    rules: {},
+};
---
+++ npm-cafe/pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="container">
+        <Logo></Logo>
+        <h1 class="title">npm-cafe</h1>
+        <div class="links">
+            <a
+                href="https://nuxtjs.org"
+                target="_brank"
+                rel="noopener noreferrer"
+                class="button--green"
+                >Documentatioin</a
+            >
+            <a
+                href="https://github.com/nuxt/nuxt"
+                target="_brank"
+                rel="noopener noreferrer"
+                class="button--green"
+            ></a>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
