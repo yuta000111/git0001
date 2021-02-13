c7857a9f8bc1fac896df29e0f94b885a6ba28322
ramirez <yuta.yamazaki@infobahn.co.jp>
2021-02-13 23:50:55 +0900

    コンポーネント作成開始

---
+++ npm-cafe/components/BaseHeading.vue
@@ -0,0 +1,19 @@
+<template>
+    <h2>こんにちは</h2>
+</template>
+<script>
+export default {
+    props: {
+        name: {
+            type: String,
+            dedault: '山田',
+        },
+    },
+};
+</script>
+<style scoped>
+h2 {
+    font-size: 30px;
+    font-weight: bold;
+}
+</style>
