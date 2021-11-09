9d5569cced99de1e6033fb5fbc3f8f5521c80fcf
ramirez <yuta.yamazaki@infobahn.co.jp>
2021-11-09 20:49:52 +0900

    renderについて学ぶ

--- react/000_handson/src/App.js
+++ react/000_handson/src/App.js
@@ -1,41 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 
-function GitHubUser({ login }) {
-  const [data, setData] = useState();
-  const [error, setError] = useState();
-  const [loading, setLoading] = useState(false);
+const tahoe_peaks = [
+  { name: 'Freel Peak', elevation: 10891 },
+  { name: 'Monument Peak', elevation: 10067 },
+  { name: 'Pyramid Peak', elevation: 9983 },
+  { name: 'Mt. Tallac', elevation: 9735 },
+];
 
-  useEffect(() => {
-    if (!login) return;
-    setLoading(true);
-    fetch(`https://api.github.com/users/${login}`)
-      .then((data) => data.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, [login]);
-
-  if (error)
-    return <pre>{JSON.stringify(error, null, 2)}</pre>;
-  if (loading) return <h1>loading...</h1>;
-  if (!data) return null;
-
-  return (
-    <div className="githubUser">
-      <img
-        src={data.avatar_url}
-        alt={data.login}
-        style={{ width: 200 }}
-      />
-      <div>
-        <h1>{data.login}</h1>
-        {data.name && <p>{data.name}</p>}
-        {data.location && <p>{data.location}</p>}
-      </div>
-    </div>
+function List({ data = [], renderItem, renderEmpty }) {
+  return !data.length ? (
+    renderEmpty
+  ) : (
+    <ul>
+      {data.map((item, i) => (
+        <li key={i}>{renderItem(item)}</li>
+      ))}
+    </ul>
   );
 }
 
 export default function App() {
-  return <GitHubUser login="moonhighway" />;
+  return (
+    <List
+      data={tahoe_peaks}
+      renderEmpty={<p>This list is empty</p>}
+      renderItem={(item) => (
+        <>
+          {item.name} - {item.elevation.toLocaleString()}
+        </>
+      )}
+    />
+  );
 }
